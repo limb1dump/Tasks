53f95cfc590691dedba20942bcddbcc18f587e08
limb1dump <skywalkervaider@gmail.com>
2022-01-31 22:15:28 +0200

    prime numbers finder code optimization

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
+// Primes finder from range
+// This programm will return in concole the list of primes from given range of special numbers
+// Prime numbers are special numbers, greater than 1, that have exactly two factors, themselves and 1.
+
+
 const start = 3;
-const end = 12;
+const end = 20;
 const result = [];
 
-let chekNumber = start; // переназначил чисто, чтоб обойти твой конст из условия:)
-const steps = end - start + 1;
-let preResult = [];
+for (let i = start; i <= end; i = i + 1) {
+    for (let index = 2; index <= i; index = index + 1) { // index = 2, because number 1 is not a prime number as it only has one factor
 
+        if ((i % index) > 0) {
+            continue;
+        } // doesnt pass special numbers that have remainder of division, but still passes numbers that doesn have remainder of division for example: (2,4,6...)
 
-for (let i = 0; i < steps; i = i + 1) {
-    for (let index = 2; index <= chekNumber; index = index + 1) {
-        if ((chekNumber % index) > 0) {
-        } else if (chekNumber / index > 1) {
+        if ((i / index) > 1) {
             break;
-        } else {
-            preResult[index - chekNumber] = index;// тут я не смог нормально вывести сразу в result (выходили пробелы между простыми) и пришлось делать костыль с преобразованием массива в строку, затем в число, затем пуш в result
-            let strMassive = preResult.join('');
-            result.push(+strMassive)
+        } // doesnt pass special numbers division result of which is > 1, because primes can only be divided by themselves and 1. As we excluded 1 from the beginning, division result has to be 1.
+
+        {
+            result.push(index)
         }
-    } //этот цикл проверяет, является ли заданное число простым (по сути может работать, как отдельная прога)
-    chekNumber = chekNumber + 1;
-
-} // этот цикл проверяет является ли каждое число простым из заданного диапазона
-
+    }
+}
 console.log(result)
+
+
